0146c66e480c40a72255d434d6d1a0fed99a6345
Muhammad Zahirul Isyraf Bin Mohamed Aidi Shahriz <85271020+mzisyraf@users.noreply.github.com>
2024-06-26 15:00:26 +0800

    Update launch_sequence.sh

--- scripts/launch_sequence.sh
+++ scripts/launch_sequence.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Launch the initial set of nodes
-roslaunch cooking_assistant robotics_project.launch &
+roslaunch robot_cooking_assistant robotics_project.launch &
 
 # Wait for the object detection to complete
 while ! rostopic echo -n1 /detection_complete | grep -q "Object detection complete"; do
@@ -9,4 +9,4 @@ while ! rostopic echo -n1 /detection_complete | grep -q "Object detection comple
 done
 
 # Launch the text to speech node
-roslaunch cooking_assistant text_to_speech.launch
+roslaunch robot_cooking_assistant text_to_speech.launch
